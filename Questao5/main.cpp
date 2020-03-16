9cf3ce41a6395a30b2b32ecc392ba6dd3dc276b6
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2020-03-16 15:59:38 -0300

    Add files via upload

---
+++ Questao5/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+int main(){
+
+    ifstream arquivo;
+    ofstream arquivo_bkp;
+    string linha;
+    arquivo.open("teste.txt");
+    arquivo_bkp.open("teste_bkp.txt");
+
+    while(!arquivo.eof()){
+    cout<< "dentro" << endl;
+    //getline(arquivo,linha);
+    arquivo >> linha;
+    //arquivo.ignore();
+    arquivo_bkp << linha << endl;
+    }
+    arquivo.close();
+    arquivo_bkp.close();
+    return 0;
+}
